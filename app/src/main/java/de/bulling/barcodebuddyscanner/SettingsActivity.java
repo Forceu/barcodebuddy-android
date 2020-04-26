3995d5d664778f079d31d5fdeab398be1018171c
Marc Ole Bulling <Marc-Ole@gmx.de>
2020-04-26 21:17:05 +0200

    Clean up, set SettingsActivity actionbar back button to close activity

--- app/src/main/java/de/bulling/barcodebuddyscanner/SettingsActivity.java
+++ app/src/main/java/de/bulling/barcodebuddyscanner/SettingsActivity.java
@@ -1,6 +1,7 @@
 package de.bulling.barcodebuddyscanner;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,15 @@ public class SettingsActivity extends AppCompatActivity {
 		if (actionBar != null) {
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	public static class SettingsFragment extends PreferenceFragmentCompat {
